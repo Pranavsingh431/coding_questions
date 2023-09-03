ef9dac04b278c28dc1ee6bed0086c283a6ae09bc
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2023-09-03 11:01:07 +0530

    answer 12

---
+++ problem12.py
@@ -0,0 +1,4 @@
+x = input("enter a string: ")#we are using the input function to add a string here.
+y  = input("enter another string: ")
+
+print(x+y) #+operator is used for concatenation of strings in python.
