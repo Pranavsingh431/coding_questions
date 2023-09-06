09110266d40016339c76bf7f8546d54f531016bb
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2023-09-06 15:34:52 +0530

    answer 15

---
+++ problem15.py
@@ -0,0 +1,5 @@
+x = input("enter a string: ")
+if x[::-1] == x: #a string is said to be palindrome if its reverse comes out to be the same as the given string.
+   print("The string is palindrome")
+else:
+   print("The string is not palindrome")
