48ac56b88a14facfb2eb598c812c49e95d017d05
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2023-09-13 01:08:09 +0530

    answer 19

---
+++ problem19.py
@@ -0,0 +1,6 @@
+#enter a number
+x = int(input("enter a number: "))
+#iterating a variable i from 1 to x+1.
+for i in range(1, x+1):
+#repeating a string "*" i number of times.    
+    print("*"*i)
