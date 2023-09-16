44166909b3125e7e7fa496cfb8bc3912ea2080a2
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2023-09-16 11:42:47 +0530

    answer 21

---
+++ problem21.py
@@ -0,0 +1,5 @@
+import math #in python there is a library called math in which there are a number of predefined functions related to maths which includes factorial.
+
+x = int(input("enter a number: "))
+
+print(math.factorial(x))
