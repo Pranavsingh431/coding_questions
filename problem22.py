98714f188eac3e95d6cabe409787c359a5798567
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2023-09-20 16:53:53 +0530

    answer 22

---
+++ problem22.py
@@ -0,0 +1,11 @@
+#entering a number
+x = int(input("enter a number: "))
+#if x>0 print that the number is positive.
+if x>0:
+   print("the number is positive")
+#if x<0 print that the number is negative.
+elif  x<0:
+   print("the number is negative")
+#else print that the number is zero.
+else:
+   print("the number is zero")
