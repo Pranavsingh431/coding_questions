9509ba66d87f07a36d91b3835925476ca3d50d88
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2023-09-27 01:03:40 +0530

    answer 23

---
+++ problem23.py
@@ -0,0 +1,6 @@
+#taking input of the p, r and t to find the simple interest.
+p = int(input("Enter a principal value: "))
+r = int(input("Enter a rate : "))
+t = int(input("Enter time(in years): "))
+#using the formula of simple interest to find the simple interest.
+print((p*r*t)/100)
