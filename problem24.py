510a62dbedba35a818fa0b1a3ad9d13eeb0b8083
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2023-10-02 18:24:48 +0530

    answer 24

---
+++ problem24.py
@@ -0,0 +1,9 @@
+#enter a temperature in celsius.
+x = float(input("Enter a temperature in celsius: "))
+
+#enter a temperature in farhenheit.
+y = float(input("Enter a temperature in farhenheit: "))
+
+#using the formula of conversion to calculate one type of temperature to another.
+print("the temperature ",x," celsius in farhenheit is ",(x*1.8)+32)
+print("the temperature ",y," ferhenheit in celsius is ",(y-32)/1.8)
