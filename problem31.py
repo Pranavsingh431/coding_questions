099627ea6ba88cc8aeb4a9fc0e6886400d879f05
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2023-10-07 23:27:01 +0530

    answer 31

---
+++ problem31.py
@@ -0,0 +1,13 @@
+#enter a number
+n = int(input("enter a number: "))
+
+#creating an empty list
+l = []
+
+#using for loop to iterate a variable i from 1 to n+1.
+for i in range(1,n+1):
+
+#adding i in the list l.
+   l.append(i)
+
+print(l)
